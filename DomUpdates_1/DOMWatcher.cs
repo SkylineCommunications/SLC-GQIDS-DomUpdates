4025deb81d92aba31642d7fd56936a7f678f55f4
Sebastiaan Dumoulein <sebastiaan.dumoulein@skyline.be>
2024-02-01 21:02:27 +0100

    Moved to separate files

---
+++ DomUpdates_1/DOMWatcher.cs
@@ -0,0 +1,43 @@
+﻿using Skyline.DataMiner.Analytics.GenericInterface;
+using Skyline.DataMiner.Net.Messages;
+using Skyline.DataMiner.Net.SubscriptionFilters;
+using Skyline.DataMiner.Net;
+using System;
+
+namespace DomUpdates_1
+{
+
+    internal class DOMWatcher : IDisposable
+    {
+        private Connection _connection = null;
+
+        internal DOMWatcher(string module, GQIDMS dms)
+        {
+            _connection = ConnectionHelper.CreateConnection(dms);
+            if (_connection == null)
+                throw new GenIfException("Could not create a connection.");
+
+            var subscriptionFilter = new ModuleEventSubscriptionFilter<DomInstancesChangedEventMessage>(module);
+            _connection.OnNewMessage += Connection_OnNewMessage;
+            _connection.AddSubscription("1", subscriptionFilter);
+            _connection.SubscribePiggyBacked(null, null);
+
+            // DOMIncidentDataSource.Log("DOMWatcher subscribed.");
+        }
+
+        internal event EventHandler<DomInstancesChangedEventMessage> OnChanged;
+
+        public void Dispose()
+        {
+            _connection.Unsubscribe();
+            _connection?.Close();
+        }
+
+        private void Connection_OnNewMessage(object sender, NewMessageEventArgs e)
+        {
+            // DOMIncidentDataSource.Log("Connection_OnNewMessage.");
+            if (e.Message is DomInstancesChangedEventMessage domChange)
+                OnChanged?.Invoke(this, domChange);
+        }
+    }
+}
--- DomUpdates_1/DomUpdates_1.cs
+++ DomUpdates_1/DomUpdates_1.cs
@@ -11,117 +11,16 @@ using Skyline.DataMiner.Net.Messages.SLDataGateway;
 using Skyline.DataMiner.Net.Sections;
 using Skyline.DataMiner.Net.LogHelpers;
 using SLDataGateway.API.Types.Results.Paging;
+using DomUpdates_1;
 
-internal static class ConnectionHelper
-{
-    private const string APPLICATION_NAME = "GQI Ad hoc data source";
-
-    internal static Connection CreateConnection(GQIDMS dms)
-    {
-        if (dms == null) throw new ArgumentNullException(nameof(dms));
-
-        var attributes = ConnectionAttributes.AllowMessageThrottling;
-        try
-        {
-            var connection = ConnectionSettings.GetConnection("localhost", attributes);
-            connection.ClientApplicationName = APPLICATION_NAME;
-            connection.AuthenticateUsingTicket(RequestCloneTicket(dms));
-            connection.SubscribePiggyBacked(null, null);
-            return connection;
-        }
-        catch (Exception ex)
-        {
-            throw new Exception("Failed to setup a connection with the DataMiner Agent: " + ex.Message, ex);
-        }
-    }
-
-    /// <summary>
-    /// Requests a one time ticket that can be used to authenticate another connection.
-    /// <see cref="AuthenticateUsingTicket(string)"/>
-    /// </summary>
-    /// <returns></returns>
-    private static string RequestCloneTicket(GQIDMS dms)
-    {
-        RequestTicketMessage requestInfo = new RequestTicketMessage(TicketType.Authentication, ExportConfig());
-        TicketResponseMessage ticketInfo = dms.SendMessage(requestInfo) as TicketResponseMessage;
-        if (ticketInfo == null)
-            throw new DataMinerException("Did not receive ticket.");
-
-        return ticketInfo.Ticket;
-    }
-
-    /// <summary>
-    /// Exports the clientside configuration for polling, zipping etc. Does not include
-    /// connection uris and the like.
-    /// </summary>
-    /// <returns></returns>
-    private static byte[] ExportConfig()
-    {
-        using (MemoryStream ms = new MemoryStream())
-        {
-            using (BinaryWriter bw = new BinaryWriter(ms))
-            {
-                bw.Write((int)1); // version
-                bw.Write(1000); // ms PollingInterval
-                bw.Write(100); // ms PollingIntervalFast
-                bw.Write(1000); // StackOverflowSize
-                bw.Write(5000); // ms ConnectionCheckingInterval
-                bw.Write(10); // MaxSimultaneousCalls
-
-                ConnectionAttributes attributesToAdd = ConnectionAttributes.AllowMessageThrottling;
-                bw.Write((int)attributesToAdd);
-
-                bw.Write("r"); // connection is remoting or IPC (which inherits from remoting)
-                bw.Write((int)1); // version
-                bw.Write(30); // s PollingFallbackTime
-            }
-
-            return ms.ToArray();
-        }
-    }
-}
-
-internal class DOMWatcher : IDisposable
-{
-    private Connection _connection = null;
-
-    internal DOMWatcher(string module, GQIDMS dms)
-    {
-        _connection = ConnectionHelper.CreateConnection(dms);
-        if (_connection == null)
-            throw new GenIfException("Could not create a connection.");
-
-        var subscriptionFilter = new ModuleEventSubscriptionFilter<DomInstancesChangedEventMessage>(module);
-        _connection.OnNewMessage += Connection_OnNewMessage;
-        _connection.AddSubscription("testink", subscriptionFilter);
-        _connection.SubscribePiggyBacked(null, null);
-        // DOMIncidentDataSource.Log("DOMWatcher subscribed.");
-    }
-
-    private void Connection_OnNewMessage(object sender, NewMessageEventArgs e)
-    {
-        // DOMIncidentDataSource.Log("Connection_OnNewMessage.");
-        if (e.Message is DomInstancesChangedEventMessage domChange)
-            OnChanged?.Invoke(this, domChange);
-    }
-
-    internal event EventHandler<DomInstancesChangedEventMessage> OnChanged;
-
-    public void Dispose()
-    {
-        _connection.Unsubscribe();
-        _connection?.Close();
-    }
-}
 
 [GQIMetaData(Name = "Incidents")]
 public class DOMIncidentDataSource : IGQIDataSource, IGQIOnInit, IGQIOnPrepareFetch, IGQIUpdateable
 {
-    private static SectionDefinitionID _sectionDefinitionID = new SectionDefinitionID(new Guid("cd2a71a1-bc75-4219-af29-05b3104cfc1f"));
-    private static FieldDescriptorID _fieldDescriptorID = new FieldDescriptorID(new Guid("b98b6a28-48cb-448f-9ad9-7414fd498f22"));
-
     private static readonly GQIStringColumn _nameColumn = new GQIStringColumn("Name");
     private static readonly GQIIntColumn _impactColumn = new GQIIntColumn("Impact");
+    private static SectionDefinitionID _sectionDefinitionID = new SectionDefinitionID(new Guid("cd2a71a1-bc75-4219-af29-05b3104cfc1f"));
+    private static FieldDescriptorID _fieldDescriptorID = new FieldDescriptorID(new Guid("b98b6a28-48cb-448f-9ad9-7414fd498f22"));
 
     private GQIDMS _dms;
     private DOMWatcher _watcher;
@@ -188,10 +87,10 @@ public class DOMIncidentDataSource : IGQIDataSource, IGQIOnInit, IGQIOnPrepareFe
         var domMetadata = new ObjectRefMetadata { Object = instance.ID };
         var rowMetadata = new GenIfRowMetadata(new[] { domMetadata });
         return new GQIRow(instance.ID.Id.ToString(), new GQICell[]
-            {
-                new GQICell() { Value = instance.Name },
-                new GQICell() { Value = instance.GetFieldValue<int>(_sectionDefinitionID, _fieldDescriptorID)?.Value}
-            })
+        {
+            new GQICell() { Value = instance.Name },
+            new GQICell() { Value = instance.GetFieldValue<int>(_sectionDefinitionID, _fieldDescriptorID)?.Value}
+        })
         { Metadata = rowMetadata };
     }
 
